130e005b09ab77644ee526291eba15a5de1816d5
Esteban <estebanmandjukowski@gmail.com>
2021-10-02 16:58:38 +0200

    nb_occs_triee O(log(n))

--- debut.ml
+++ debut.ml
@@ -429,3 +429,33 @@ let rec tri_fusion t =
   match t with
       | [] | [_] -> t
       |_ -> let a,b = eclate t in fusion (tri_fusion a) (tri_fusion b)
+
+
+let premiere_occ t x =
+  let rec aux deb fin =
+      if deb > fin then -1 else
+      if deb = fin then deb
+      else
+          let milieu = ((deb+fin)/2) in
+              if t.(milieu) = x then aux deb milieu
+              else if t.(milieu) < x then aux (milieu + 1) fin
+              else aux deb milieu
+      in aux 0 (Array.length t)
+
+
+let derniere_occ t x =
+  let rec aux deb fin =
+      if deb > fin then -1 else
+      if deb = (fin-1) then deb
+      else
+          let milieu = ((deb+fin)/2) in
+              if t.(milieu) = x then aux milieu fin
+              else if t.(milieu) < x then aux milieu fin
+              else aux deb milieu
+      in aux 0 (Array.length t)
+
+let nb_occs_triee elt tbl =
+  let first_index = premiere_occ tbl elt in
+  let last_index = derniere_occ tbl elt in
+  if first_index = -1 || last_index = -1 then failwith "Element not in the list."
+  else (last_index - first_index) + 1
