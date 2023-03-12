1c6fa1a9605ea25fb21ff29c6bfcfe8a73de827a
Esteban <estebanmandjukowski@gmail.com>
2023-03-12 19:48:38 +0100

    parse_regex_2 done

--- tds/2A/tp20_parsing/tp20.ml
+++ tds/2A/tp20_parsing/tp20.ml
@@ -7,7 +7,7 @@ type state = {
 
 let new_state str = {
   str = str;
- index = 0
+  index = 0
 }
 
 exception SyntaxError 
@@ -32,6 +32,7 @@ let rec expect stream c =
     stream.index <- stream.index + 1;
   | _ -> printf "Expected %c\n" c; error stream
 
+
 let discard stream = 
   if stream.index = String.length stream.str then error stream
   else stream.index <- stream.index + 1
@@ -39,6 +40,8 @@ let discard stream =
 let is_letter c = 
   let num = int_of_char c in 
   num - int_of_char 'A' > 0 && int_of_char 'z' - num > 0
+
+
 
 type regex_ast = 
   |Sum of regex_ast * regex_ast
@@ -53,22 +56,24 @@ let rec regex stream =
   match peek stream with 
   |Some '(' -> 
     begin
+
     let p = parenthese stream in 
     match peek stream with 
-    |Some '+' -> 
-      discard stream;
-      let temp_p = parenthese stream in
-      Sum (p,temp_p)
-    | Some '*' -> 
-      discard stream;
-      Star p;
-    |Some '?' ->
-      discard stream;
-      Maybe p;
-    |Some '(' ->
-      let temp_p = parenthese stream in
-      Concat (p,temp_p) 
-    |_ -> failwith "pattern matching moment"
+      |Some '+' -> 
+        discard stream;
+        let temp_p = parenthese stream in (*On process le reste de l'expression*)
+        Sum (p,temp_p)
+      | Some '*' -> 
+        discard stream;
+        Star p;
+      |Some '?' ->
+        discard stream;
+        Maybe p;
+      |Some '(' ->
+        let temp_p = parenthese stream in
+        Concat (p,temp_p) 
+      |_ -> failwith "pattern matching moment"
+
     end
   |Some '.' -> 
     discard stream;
@@ -89,14 +94,43 @@ parenthese s =
   | _ -> error s (*pattern matching pas exhaustif sinon*)
 
 
-let parse_regex stream = 
-  let s = new_state stream in 
-  let t = regex s in 
-  if s.index < String.length stream then 
-    begin
-    printf "Unexpected input to end\n";
-    error s
-    end 
-  else t
+let parse initial_symbol str =
+  let s = new_state str in
+  let tree = initial_symbol s in
+  match peek s with
+  | None -> tree
+  | Some c -> printf "Expected input to end\n"; error s
 
   
+let parse_regex str = parse regex str
+
+
+let rec regex2 (s : state) = 
+  let t = term s in 
+  match peek s with 
+  |Some '+' -> 
+    discard s;
+    let e = regex2 s in 
+    Sum (t,e)
+  | _ -> t
+  and term s =
+    let f = factor s in 
+    match peek s with 
+    | None | Some ')' | Some '+' -> f 
+    | _ -> 
+      let t = term s in 
+      Concat (f, t)
+
+  and factor s = 
+      match peek s with
+      | Some '(' -> discard s; 
+      let e = regex2 s in
+      expect s ')';
+      e
+      | Some c when is_letter c -> discard s; Char c
+      | Some '.' -> discard s; Any
+      | _ -> error s
+
+
+let parse_regex_2 str = 
+  parse regex2 str
